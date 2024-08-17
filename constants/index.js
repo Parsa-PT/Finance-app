ccedef1a5ad9ff00177a476877c012b21dcaa891
Parsapt <90087314+Parsa-PT@users.noreply.github.com>
2024-08-17 21:15:09 +0330

    Add files via upload

---
+++ constants/index.js
@@ -0,0 +1,51 @@
+export const EXPENSE = [
+    {
+        title:'Housing',
+        amount:'955.55',
+        percent:'60'
+    },
+    {
+        title:'Food',
+        amount:'790.41',
+        percent:'13'
+    },
+    {
+        title:'Saving',
+        amount:'350.56',
+        percent:'20'
+    },
+    {
+        title:'Bank',
+        amount:'484.22',
+        percent:'10'
+    },
+    {
+        title:'Test',
+        amount:'781.16',
+        percent:'12'
+    },
+ 
+]
+
+
+export const INCOME = [
+    {
+        title:'Salary',
+        amount:'1,500.00',
+    },
+    {
+        title:'Freelancing',
+        amount:'850',
+    },
+    {
+        title:'Interset',
+        amount:'500',
+    },
+]
+
+export const SPEND = [
+    {
+        title:'',
+        amount:''
+    },
+]
