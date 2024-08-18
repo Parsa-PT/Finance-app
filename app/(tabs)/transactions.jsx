ef0f0446418e4bf2be5ad80d180cb26a61f850f4
Parsapt <90087314+Parsa-PT@users.noreply.github.com>
2024-08-18 20:20:52 +0330

    Update transactions.jsx

--- app/(tabs)/transactions.jsx
+++ app/(tabs)/transactions.jsx
@@ -1,10 +1,26 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { StyleSheet } from 'react-native'
+import { Colors } from '../../constants/Colors'
+
 
 export default function transactions() {
   return (
-    <View>
+    <>
+    
+      <View style={styles.container}>
       <Text>transactions</Text>
     </View>
+    </>
   )
 }
+
+
+const styles = StyleSheet.create({
+  container : {
+      flex:1,
+      backgroundColor:Colors.light,
+      paddingHorizontal:30,
+      
+  }
+})
